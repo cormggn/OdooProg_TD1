6230058c383edcca93c78b2dc5b2622e80d51992
Cormac <X00179608@mytudublin.ie>
2023-03-08 17:39:32 +0100

    first commit

---
+++ odoo/local-src/students/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Gestion des étudiants",
+    "version": "0.8",
+    "category": "Generic Modules/Others",
+    "description": """Test création module gestion des étudiants Odoo v14""",
+    "author": "goganc",
+    "data": ["views/students_views.xml"],
+    "depends": ["base"],
+    "installable": True,
+    "auto_install": False,
+}
